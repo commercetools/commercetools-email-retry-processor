b212aa134c5eaa84a595c25a982cba725f5aef20
Andreas Halberkamp <ahalberkamp@ct-00578.local>
2017-11-13 15:10:05 +0100

    Car-2012:Add EmailProcessor

--- src/main/java/com/commercetools/emailprocessor/Main.java
+++ src/main/java/com/commercetools/emailprocessor/Main.java
@@ -1,7 +1,9 @@
 package com.commercetools.emailprocessor;
 
 
+import com.commercetools.emailprocessor.email.EmailProcessor;
 import com.commercetools.emailprocessor.model.ProjectConfiguration;
+import com.commercetools.emailprocessor.model.TenantConfiguration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.sphere.sdk.json.SphereJsonUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -10,6 +12,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.temporal.TemporalAccessor;
 
 
 public class Main {
@@ -23,31 +26,35 @@ public class Main {
 
     public static void main(final String[] args) {
         ObjectMapper objectMapper = new ObjectMapper();
-        ProjectConfiguration config = null;
+        ProjectConfiguration projectConfiguration = null;
         try {
 
-        if (args != null && args.length > 0) {
-            String resourcePath = args[0];
-            File file = new File(resourcePath);
+            if (args != null && args.length > 0) {
+                String resourcePath = args[0];
+                File file = new File(resourcePath);
 
-            try {
-                config = objectMapper.readValue(file, ProjectConfiguration.class);
-            } catch (IOException e) {
-                LOGGER.error("The File cannot be parsed", e);
+                try {
+                    projectConfiguration = objectMapper.readValue(file, ProjectConfiguration.class);
+                } catch (IOException e) {
+                    LOGGER.error("The File cannot be parsed", e);
+                }
+
+            } else {
+                final String ctpProjectConfig = System.getenv(CTP_PROJECT_CONFIG);
+                if (StringUtils.isNotEmpty(ctpProjectConfig)) {
+                    projectConfiguration = objectMapper.readValue(ctpProjectConfig, ProjectConfiguration.class);
+                }
             }
-
-        } else {
-            final String ctpProjectConfig = System.getenv(CTP_PROJECT_CONFIG);
-            if (StringUtils.isNotEmpty(ctpProjectConfig)) {
-
-                config =objectMapper.readValue(ctpProjectConfig, ProjectConfiguration.class);
-            }
+        } catch (IOException e) {
+            LOGGER.error("The json stream cannot be parsed", e);
         }
-    } catch (IOException e) {
-        LOGGER.error("The File cannot be parsed", e);
-    }
-        if (config != null && config.isValid()) {
-            LOGGER.error("A valid config was found");
+        if (projectConfiguration != null && projectConfiguration.isValid()) {
+            for (TenantConfiguration tenantConfig : projectConfiguration.getTenants()) {
+                boolean success = EmailProcessor.processEmails(tenantConfig);
+                if (!success) {
+                    break;
+                }
+            }
         } else {
             LOGGER.error("NO valid config was found");
 
--- src/main/java/com/commercetools/emailprocessor/email/EmailProcessor.java
+++ src/main/java/com/commercetools/emailprocessor/email/EmailProcessor.java
@@ -1,16 +1,102 @@
 package com.commercetools.emailprocessor.email;
 
+import com.commercetools.emailprocessor.model.TenantConfiguration;
+import com.fasterxml.jackson.databind.JsonNode;
+import io.sphere.sdk.client.SphereClient;
+import io.sphere.sdk.customobjects.CustomObject;
+import io.sphere.sdk.customobjects.queries.CustomObjectQuery;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletionStage;
+
 public class EmailProcessor {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(EmailProcessor.class);
+    private static final Logger LOG = LoggerFactory.getLogger(EmailProcessor.class);
+    private static final String PARAM_EMAIL_ID = "emailid";
+    private static final String PARAM_TENANT_ID = "tenantid";
+    private static final String CONTAINER_ID = "unprocessedEmail";
+    private static final String EMAIL_PROPERTY_STATUS = "status";
+    private static final String EMAIL_STATUS_PENDING = "pending";
+
+    public static final int RESPONSE_CODE_SUCCESS = 201;
+    public static final int RESPONSE_ERROR_TEMP = 200;
 
 
-        public static void sendEmails() {
-        }
+    public static boolean processEmails(TenantConfiguration tenantConfiguration) {
+        boolean success = true;
+        SphereClient client = tenantConfiguration.getSphereClient();
+        CustomObjectQuery<JsonNode> query = CustomObjectQuery.ofJsonNode();
+        query = query.byContainer(CONTAINER_ID);
+        success = client.execute(query).thenApply(r -> {
+                    if (r.getTotal() < 1) {
+                        LOG.error(String.format("No email to process for tenant %s", tenantConfiguration
+                                .getProjectKey()));
+                    }
+                    boolean processed = true;
+                    int sendEmails = 0;
+                    int send_successfull = 0;
+                    int sendErrorTemp = 0;
+                    int sendErrorPermanent = 0;
+                    for (CustomObject<JsonNode> customObject : r.getResults()) {
+                        JsonNode email = customObject.getValue();
+                        String status = email != null && email.get(EMAIL_PROPERTY_STATUS) != null ? email.get
+                                (EMAIL_PROPERTY_STATUS).asText() : "";
+                        if (StringUtils.equalsIgnoreCase(status, EMAIL_STATUS_PENDING)) {
+                            int responseCode = sendEmail(customObject, tenantConfiguration);
+                            sendEmails++;
+                            switch (responseCode) {
+                                case RESPONSE_CODE_SUCCESS:
+                                    send_successfull++;
+                                    break;
+                                case RESPONSE_ERROR_TEMP:
+                                    sendErrorTemp++;
+                                    break;
+                                default:
+                                    sendErrorPermanent++;
+                                    break;
+                            }
+                        }
+                    }
+                    LOG.info("##########################");
+                    LOG.info(String.format("Processing statistic for tenant %s", tenantConfiguration.getProjectKey()));
+                    LOG.info("# processed Email" + sendEmails);
+                    LOG.info("# processed successfull" + send_successfull);
+                    LOG.info("# processed with temporal error" + sendErrorTemp);
+                    LOG.info("#processed with permanent error" + send_successfull);
+                    LOG.info("##########################");
+                    return processed;
+                }
 
+        ).toCompletableFuture().join();
+        return success;
     }
 
 
+     static int sendEmail(CustomObject<JsonNode> customObject, TenantConfiguration tenantConfiguration) {
+        int responseCode = HttpStatus.SC_OK;
+        try {
+            URL postUrl = new URL(tenantConfiguration.getWebhookURL());
+            HttpURLConnection con = (HttpURLConnection) postUrl.openConnection();
+            con.setRequestMethod("POST");
+            con.setDoOutput(true);
+            OutputStream outputStream = con.getOutputStream();
+            String params = String.format(PARAM_EMAIL_ID + "=%s&" + PARAM_TENANT_ID + "=%s", customObject.getId(),
+                    tenantConfiguration.getProjectKey());
+            outputStream.write(params.getBytes());
+            outputStream.flush();
+            outputStream.close();
+            responseCode = con.getResponseCode();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return responseCode;
+    }
+}
