76ae8774017141c2e93bc4ad0604d060f24c5f6e
Andreas Halberkamp <ahalberkamp@ct-00578.local>
2017-11-29 15:05:10 +0100

    disable accepting single quote in json files.

--- src/main/java/com/commercetools/emailprocessor/utils/ConfigurationUtils.java
+++ src/main/java/com/commercetools/emailprocessor/utils/ConfigurationUtils.java
@@ -6,12 +6,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Optional;
 
-import static com.fasterxml.jackson.core.JsonParser.Feature.ALLOW_SINGLE_QUOTES;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
 
@@ -28,8 +26,6 @@ public class ConfigurationUtils {
      */
     public static Optional<ProjectConfiguration> getConfiguration(final String resourcePath) {
         final ObjectMapper objectMapper = new ObjectMapper();
-        //Allows single Quotes within the json files
-        objectMapper.configure(ALLOW_SINGLE_QUOTES, true);
         ProjectConfiguration projectConfiguration = null;
         try {
             if (StringUtils.isNotBlank(resourcePath)) {
