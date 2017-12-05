29b2c966d7cb4d2a0256f8ebe950a330334456b6
Andreas Halberkamp <ahalberkamp@ct-00578.local>
2017-12-05 16:04:34 +0100

    Improve formatting.

--- src/main/java/com/commercetools/emailprocessor/jobs/EmailJob.java
+++ src/main/java/com/commercetools/emailprocessor/jobs/EmailJob.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -28,28 +27,25 @@ public class EmailJob {
      * @param projectConfiguration configuration of a given Project
      */
     public static List<Statistics> process(@Nonnull final ProjectConfiguration projectConfiguration) {
-        if (projectConfiguration != null) {
-            final EmailProcessor emailProcessor = new EmailProcessor();
-            final List<CompletableFuture<Statistics>> listOfStageOfStatistics = projectConfiguration.getTenants()
-                .parallelStream()
-                .map(tenantConfiguration -> {
-                        try {
-                            return emailProcessor.processEmails(tenantConfiguration).toCompletableFuture();
-                        } catch (Exception exception) {
-                            LOG.error(String.format("Error in email processing for tenant %s.",
-                                tenantConfiguration.getProjectKey()), exception);
-                        }
-                        return completedFuture(new Statistics());
-                        }
-                )
-                .collect(toList());
-            return allOf(listOfStageOfStatistics.toArray(new CompletableFuture[listOfStageOfStatistics.size()]))
-                .thenApply(ignoreVoid -> listOfStageOfStatistics.stream()
-                    .map(CompletableFuture::join)
-                    .collect(toList()))
-                .toCompletableFuture().join();
-        }
-        LOG.error("Invalid project configuration!");
-        return Collections.emptyList();
+
+        final EmailProcessor emailProcessor = new EmailProcessor();
+        final List<CompletableFuture<Statistics>> listOfStageOfStatistics = projectConfiguration.getTenants()
+            .parallelStream()
+            .map(tenantConfiguration -> {
+                    try {
+                        return emailProcessor.processEmails(tenantConfiguration).toCompletableFuture();
+                    } catch (Exception exception) {
+                        LOG.error(String.format("Error in email processing for tenant %s.",
+                            tenantConfiguration.getProjectKey()), exception);
+                    }
+                    return completedFuture(new Statistics());
+                    }
+            )
+            .collect(toList());
+        return allOf(listOfStageOfStatistics.toArray(new CompletableFuture[listOfStageOfStatistics.size()]))
+            .thenApply(ignoreVoid -> listOfStageOfStatistics.stream()
+                .map(CompletableFuture::join)
+                .collect(toList()))
+            .toCompletableFuture().join();
     }
 }
