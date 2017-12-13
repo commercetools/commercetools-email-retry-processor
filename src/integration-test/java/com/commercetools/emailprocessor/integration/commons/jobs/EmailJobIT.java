bd22992bea2354e2e6e382fe52a18a3d88a29596
Andreas Halberkamp <ahalberkamp@ct-00578.local>
2017-12-13 15:34:20 +0100

    Change package name.

--- src/integration-test/java/com/commercetools/emailprocessor/integration/commons/jobs/EmailJobIT.java
+++ src/integration-test/java/com/commercetools/emailprocessor/integration/commons/jobs/EmailJobIT.java
@@ -37,7 +37,7 @@ import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
-public class EmailJobIT {
+public class stEmailJobIT {
     private static final Logger LOG = LoggerFactory.getLogger(EmailJobIT.class);
     private static final String HTTPBIN_DOMAIN = "https://httpbin.org/status/";
     @Rule
