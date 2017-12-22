9457fe5ffacc6a6cc33024da702d89bdda5679c2
Andreas Halberkamp <ahalberkamp@ct-00578.local>
2017-12-22 10:14:55 +0100

    remove unused import.

--- src/test/java/com/commercetools/emailprocessor/email/EmailProcessorTest.java
+++ src/test/java/com/commercetools/emailprocessor/email/EmailProcessorTest.java
@@ -37,7 +37,6 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
